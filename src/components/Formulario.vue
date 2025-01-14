a973cf3363ef6e3f60842f67b900795364194c24
Raquel Vuicik <raquel.vuicik@artemidas.net>
2025-01-14 10:10:49 -0300

    refatorando tempoDecorrido cronometro

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -14,11 +14,7 @@
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justity-content-space-between">
-                    <section>
-                        <strong>
-                            {{ tempoDecorrido }}
-                        </strong>
-                    </section>
+                    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
                     <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
@@ -40,19 +36,18 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
 
 export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Formulario',
+    components: {
+        Cronometro
+    },
     data () {
         return {
             tempoEmSegundos: 0,
             cronometro: 0
-        }
-    },
-    computed: {
-        tempoDecorrido () : string {
-            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
         }
     },
     methods: {
