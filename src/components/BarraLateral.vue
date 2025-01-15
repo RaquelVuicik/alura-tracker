e9de42b4476695d22781654f859068dc1e400f51
Raquel Vuicik <raquel.vuicik@artemidas.net>
2025-01-15 15:47:39 -0300

    adicionando rota para projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,11 +1,29 @@
 <template>
     <header>
         <h1>
-            <img src="../assets/logo.png" alt="">
+            <img src="../assets/logo.png" alt="Alura Tracker">
         </h1>
-        <button class="button" @click="alterarTema" >
-          {{ textoBotao }}
-        </button>
+        <div class="has-text-centered" >
+          <button class="button" @click="alterarTema" >
+            {{ textoBotao }}
+          </button>
+        </div>
+        <nav class="panel mt-5" >
+          <ul>
+            <li>
+              <router-link to="/" class="link">
+                <i class="fas fa-tasks"></i>
+                tarefas
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/projetos" class="link">
+                <i class="fas fa-project-diagram"></i>
+                projetos
+              </router-link>
+            </li>
+          </ul>
+        </nav>
     </header>
 </template>
 
@@ -39,8 +57,14 @@ export default defineComponent({
 
 <style scoped>
 /* todo o estilo do componente */
+h1 {
+  text-align: center;
+}
+strong {
+  color: #f95738;
+}
 header {
-  padding: 1rem;
+  padding: 2rem;
   background: #0d3b66;
   width: 100%;
   height: 100vh;
@@ -48,8 +72,19 @@ header {
 }
 @media only screen and (max-width: 768px) {
   header {
-    padding: 2.5rem;
     height: auto;
   }
+}
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #FAF0CA;
+}
+.link.router-link-active {
+  color: #FAF0CA;
 }
 </style>
