d673af138322ee6b9563ed34f94e90470b36e16d
Raquel Vuicik <raquel.vuicik@artemidas.net>
2025-01-15 10:10:36 -0300

    criando componente Box

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <Box>
     <div class="columns">
       <div class="column is-7">
-        {{ tarefa.descricao }}
+        {{ tarefa.descricao || 'Tarefa sem descrição' }}
       </div>
       <div class="column">
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
       </div>
     </div>
-  </div>
+  </Box>
 </template>
 
 <script lang="ts">
 import ITarefa from '@/interfaces/ITarefa';
 import Cronometro from './Cronometro.vue';
 import { defineComponent, PropType } from 'vue';
+import Box from './Box.vue';
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Tarefa',
   components: {
-    Cronometro
+    Cronometro,
+    Box
   },
   props: {
     tarefa: {
@@ -31,9 +33,3 @@ export default defineComponent({
 })
 
 </script>
-
-<style scoped>
-.box {
-  background: #FAF0CA;
-}
-</style>
